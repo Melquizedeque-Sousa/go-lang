e16f5f07c51a3015b67e1aa88b4fa656b4ae24a1
Melquizedeque Sousa <melquizedequesousa50@gmail.com>
2026-01-18 16:29:03 -0300

    Add files via upload

--- javascript.js
+++ javascript.js
@@ -1,1 +1,10 @@
-window.alert('bem vindo!');
+// Pegar elemento pelo id
+const input = document.getElementById('input') ;
+const button = document.getElementById('button');
+button.addEventListener('click', function(e){
+    const text = input.value;
+    alert(`Olá, meu joven ${text}`)
+    if (text) alert(`Olá, ${text} `)
+    else alert(`Nenhum nome informado.`)
+})
+
